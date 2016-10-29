298867604634cffe9d7db342d1a374fbf3f81170
kvance12 <katy.vance12@gmail.com>
2016-10-29 09:36:58 -0600

    test

---
+++ FirstMVCSite4/FirstMVCSite4/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using FirstMVCSite4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCSite4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("ThankYou", response);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FirstMVCSite4/FirstMVCSite4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMVCSite4.MvcApplication" Language="C#" %>
